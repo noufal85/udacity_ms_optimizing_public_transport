3b8f2d502ca4d7015461bec521fd72ee1f9c4f23
Noufal E <noufal85@gmail.com>
2026-08-03 10:04:38 -0700

    Produce turnstile events keyed by station ID

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -13,30 +13,16 @@ logger = logging.getLogger(__name__)
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
     value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
-
 
     def __init__(self, station):
         """Create the Turnstile"""
-        station_name = (
-            station.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
-
-        #
-        #
-        #
-        #
         super().__init__(
             "org.chicago.cta.station.turnstiles.v1",
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=3,
-            num_replicas=1
+            num_replicas=1,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -44,15 +30,11 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        #
-        #
-        #
-        #
         for _ in range(num_entries):
             self.producer.poll(0)
             self.producer.produce(
                 topic=self.topic_name,
-                key={"timestamp": self.time_millis()},
+                key={"station_id": self.station.station_id},
                 value={
                     "station_id": self.station.station_id,
                     "station_name": self.station.name,
